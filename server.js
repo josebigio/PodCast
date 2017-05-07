91afd3d413deaf126de5684a7f2bb423cdfa8f8a
Jose Bigio <sirownsalot@gmail.com>
2017-05-07 19:35:46 -0400

    added logs

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const indexPath = path.join(__dirname, '/index.html');
+console.log('indexpath',indexPath);
 const bundle = path.join(__dirname, '/bundle.js');
+console.log('bundle',bundle);
 const port = process.env.PORT || 8080;
+console.log('port',port);
 
 app.get('/bundle.js',(req,res)=>{
+    console.log('get /bundle.js',req);
     res.sendFile(bundle);
 });
 app.get('/',(req,res)=>{
+    console.log('get /',req);
     res.sendFile(indexPath);
 });
 app.listen(port);
