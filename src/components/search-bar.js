2711eb4eb78cd2f38e1d507268e5a46f69212595
Jose Bigio <sirownsalot@gmail.com>
2017-05-21 20:54:21 -0400

    imrpoved UI

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import FontAwesome from 'react-fontawesome';
 import { bindActionCreators } from 'redux';
 import { searchPodCast, onSearchResultClicked } from '../actions'
 import SearchResultList from './search-result-list';
@@ -23,18 +24,28 @@ class SearchBar extends Component {
         return (parseInt(ratings.likeCount)/(parseInt(ratings.likeCount) + parseInt(ratings.dislikeCount))*100).toFixed(2);
     }
 
+    handleDropDownClick() {
+        console.log('handleDropDownClick');
+    }
+
     render() {
         return (
             <div className="search-bar">
                 <h6 style={{textAlign:"center"}}>{this.props.currentEpisode}</h6>
                 <p1>{this.props.ratings && this.getApproval(this.props.ratings) + "%"}</p1>
                 <div className="search-input-section">
+                    <div className="search-input-wrapper">
                     <input type="search"
                         className="form-control search-input"
                         placeholder="Search for a JRE podcast"
                         value={this.props.inputValue}
-                        onChange={this.handleInputChange.bind(this)} />
-
+                        onChange={this.handleInputChange.bind(this)} >
+                        </input>
+                         <FontAwesome className="search-icon" 
+                            onClick={this.handleDropDownClick.bind(this)}
+                            name="caret-down" />
+                        </div>
+    
                     {this.props.inputValue.length > 0 &&
                         <SearchResultList
                             resultList={this.props.searchResultList}
